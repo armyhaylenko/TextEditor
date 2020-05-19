3c4e68ca3472de342db898f87d445d943793c813
armyhaylenko <armyhaylenko@gmail.com>
2020-05-19 19:12:22 +0300

    Initial commit

--- src/main/scala/BasicLineChart.scala
+++ src/main/scala/BasicLineChart.scala
@@ -7,68 +7,100 @@ import scalafx.collections.ObservableBuffer
 import scalafx.embed.swing.SwingFXUtils
 import scalafx.scene.Scene
 import scalafx.scene.chart.{LineChart, NumberAxis, XYChart}
-import scalafx.scene.control.{Menu, MenuBar, MenuItem, SeparatorMenuItem}
+import scalafx.scene.control._
 import scalafx.scene.image.WritableImage
-import scalafx.scene.layout.BorderPane
+import scalafx.scene.layout.{BorderPane, HBox}
 import scalafx.stage.FileChooser
+
 
 object BasicLineChart extends JFXApp {
 
 
   stage = new JFXApp.PrimaryStage {
     title = "Line Chart Example"
-    scene = new Scene {
-      val lineChart = {
+    scene = new Scene(800, 600) {
+      val menu = {
+        val menuBar = new MenuBar
+        val saveMenu = new Menu("Save")
+        val save = new MenuItem("Save as")
+        save.onAction = _ => {
+          val extFilter = new FileChooser.ExtensionFilter("Image",  Seq("*.png, *.jpg"))
+          val fc = new FileChooser {
+            extensionFilters :+ extFilter
+            title = "Save your plot:"
+          }
+          val file = fc.showSaveDialog(stage)
+          savePng(file)
+        }
+        val exit = new MenuItem("Exit")
+        exit.onAction = _ => {
+          sys.exit(0)
+        }
+        saveMenu.items = List(save, new SeparatorMenuItem, exit)
+        menuBar.menus = List(saveMenu)
+        menuBar
+      }
 
-        val xAxis = NumberAxis("Values for X-Axis", -3 * math.Pi, 3 * math.Pi, 0.1)
-        val yAxis = NumberAxis("Values for Y-Axis", -1.5, 1.5, 1)
+      val textFieldsPane = new HBox(20) {
+        val enterRange = new TextField{
+          text = "Enter range:"
+        }
+        val enterA = new TextField{
+          text = "Enter a:"
+        }
+        val enterB = new TextField{
+          text = "Enter b:"
+        }
+        children = List(enterRange, enterA, enterB)
+      }
+
+      lazy val lineChart = {
+        val a = textFieldsPane.enterA.getText.toDouble
+        val b = textFieldsPane.enterB.getText.toDouble
+
+        val xAxis = NumberAxis("Values for X-Axis", -3*(b + a), 5 + b + a, 0.1)
+        val yAxis = NumberAxis("Values for Y-Axis", -1.5 - b - a, 1.5 + b + a, 1)
+        //parse range
+        val range = textFieldsPane.enterRange.getText.split(";").map{
+          case expr if expr contains "*" =>
+            val split = expr.split("\\*").map{
+              case pi if pi contains "pi" => math.Pi
+              case num => num.toDouble
+            }
+            BigDecimal(split(0) * split(1))
+          case pi if pi contains "pi" => BigDecimal(math.Pi)
+          case num => BigDecimal(num.toDouble)
+        }
 
         // Helper function to convert a tuple to `XYChart.Data`
         val toChartData = (xy: (Double, Double)) => XYChart.Data[Number, Number](xy._1, xy._2)
-
         val series1 = new XYChart.Series[Number, Number] {
-          name = "Series 1"
-          val sin = for (i <- BigDecimal(-2 * math.Pi) to(BigDecimal(2 * math.Pi), 0.1))
-            yield (i.toDouble, math.sin(i.toDouble))
-          data = sin.map(toChartData)
+          name = "Limacon"
+          val loop = for (i <- range(0).to(range(1), 0.1))
+            yield (a / 2 + b * math.cos(i.toDouble) + (a * math.cos(2 * i.toDouble) / 2),
+              b * math.sin(i.toDouble) + (a * math.sin(2 * i.toDouble) / 2))
+          data = loop.map(toChartData)
         }
-
-
         val lineChart = new LineChart[Number, Number](xAxis, yAxis, ObservableBuffer(series1))
-        lineChart.setAnimated(false)
-
-
         lineChart
-
       }
 
       def savePng(file: File): Unit = {
         val img = lineChart.snapshot(null, new WritableImage(500, 400))
         ImageIO.write(SwingFXUtils.fromFXImage(img, null), "png", file)
       }
-
-      val menuBar = new MenuBar
-      val saveMenu = new Menu("Save")
-      val save = new MenuItem("Save")
-      save.onAction = (_) => {
-        val extFilter = new FileChooser.ExtensionFilter("Image",  Seq("*.png, *.jpg"))
-        val fc = new FileChooser {
-          extensionFilters :+ extFilter
-          title = "Save your plot:"
+      val calculate = new Button("CALCULATE!!"){
+        onAction = _ => {
+          var mutableLineChart = lineChart
+          rootPane.center = mutableLineChart
         }
-        val file = fc.showSaveDialog(stage)
-        savePng(file)
+        layoutX = 700
+        layoutY = 550
       }
-      val exit = new MenuItem("Exit")
-      exit.onAction = (_) => {
-        sys.exit(0)
-      }
-      saveMenu.items = List(save, new SeparatorMenuItem, exit)
-      menuBar.menus = List(saveMenu)
-
       val rootPane = new BorderPane
-      rootPane.top = menuBar
-      rootPane.center = lineChart
+      rootPane.top = menu
+      rootPane.bottom = textFieldsPane
+      rootPane.right = calculate
       root = rootPane
     }
   }
